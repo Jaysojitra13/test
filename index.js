11a343793e4b84747f4770d765ebd8575b93308e
Hemil Patel <hemil.patel@solutionanalysts.com>
2018-10-10 19:40:29 +0530

    simple commit

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.post('/',(req, res) => {
     res.status(200).json({ message: 'message from 7001'});
 });
 
+app.all('/*', (req, res) => {
+    return res.status(401).json({
+      error: "Url not found",
+    });
+  });
+
 app.listen(7000, (err, res) => {
     if(err)  console.log(err)
     console.log("server is running..")
